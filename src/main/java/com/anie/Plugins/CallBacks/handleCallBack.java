a98e8a09cc2f567887d2052ef9aa658d849decc4
Aaryan <aaryan14032006@gmail.com>
2021-10-07 14:35:25 +0530

    Updated

--- src/main/java/com/anie/Plugins/CallBacks/handleCallBack.java
+++ src/main/java/com/anie/Plugins/CallBacks/handleCallBack.java
@@ -1,10 +1,13 @@
 package com.anie.Plugins.CallBacks;
 
+import java.util.ArrayList;
 import com.anie.Anie;
 import com.anie.Master;
+import org.telegram.telegrambots.meta.api.methods.groupadministration.GetChatAdministrators;
 import org.telegram.telegrambots.meta.api.methods.groupadministration.UnbanChatMember;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.chatmember.ChatMember;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class handleCallBack extends Anie implements Master {
@@ -16,33 +19,36 @@ public class handleCallBack extends Anie implements Master {
             UnbanChatMember unbanChatMember = new UnbanChatMember(
                     update.getCallbackQuery().getMessage().getChatId().toString(), uid);
             try {
-                execute(unbanChatMember);
-                SendMessage msg = new SendMessage(update.getCallbackQuery().getMessage().getChatId().toString(),
-                        "Successfully Unbanned!\nI wish he would not do the same Again ;)");
-                execute(msg);
+                if (isAdmin(update)) {
+                    execute(unbanChatMember);
+                    SendMessage msg = new SendMessage(update.getCallbackQuery().getMessage().getChatId().toString(),
+                            "Successfully Unbanned!\nI wish he would not do the same Again ;)");
+                    execute(msg);
+                }
             } catch (TelegramApiException e) {
                 sendMessage(update, e.getMessage());
             }
         }
 
     }
-    /*
-     * public boolean isAdmin(Update update) { GetChatAdministrators administrators
-     * = new GetChatAdministrators(
-     * update.getCallbackQuery().getMessage().getChatId().toString());
-     * ArrayList<ChatMember> members;
-     * 
-     * ChatMember member = new
-     * ChatMemberMember(update.getCallbackQuery().getFrom());
-     * System.out.println(member); if (member.getStatus().equals("administrator") ||
-     * member.getStatus().equals("creator")) { return true; } try { members =
-     * execute(administrators); for (ChatMember chm : members) { if(chm.equals()) if
-     * (member.getStatus().equals("administrator") ||
-     * member.getStatus().equals("creator")) { return true; } } } catch
-     * (TelegramApiException e) { sendMessage(update, e.getMessage()); } return
-     * false;
-     * 
-     * }
-     */
+
+    public boolean isAdmin(Update update) {
+        GetChatAdministrators administrators = new GetChatAdministrators(
+                update.getCallbackQuery().getMessage().getChatId().toString());
+        ArrayList<ChatMember> members;
+        try {
+            members = execute(administrators);
+            for (ChatMember chm : members) {
+                long id = Integer.parseInt(chm.toString().substring(chm.toString().indexOf("id")+3, chm.toString().indexOf(",", chm.toString().indexOf("id"))));
+                if(update.getCallbackQuery().getFrom().getId() == id) {
+                    return true;
+                }
+            }
+        } catch (TelegramApiException e) {
+            sendMessage(update, e.getMessage());
+        }
+        return false;
+
+    }
 
 }
