6405b8a5487c88a651ba113b179b2f6c44d3e868
Aaryan <aaryan14032006@gmail.com>
2021-10-07 19:46:46 +0530

    Created Kickme Plugin

---
+++ src/main/java/com/anie/Plugins/kickme.java
@@ -0,0 +1,16 @@
+package com.anie.Plugins;
+
+import com.anie.Anie;
+import com.anie.Master;
+
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class kickme extends Anie implements Master {
+
+    @Override
+    public void handleRequest(Update update, String cmd) {
+        
+        
+    }
+    
+}
