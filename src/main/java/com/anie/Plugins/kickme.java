0d001cc66d1a3014be5833af6aabeb771c5a6c16
Aaryan <aaryan14032006@gmail.com>
2021-10-07 19:52:54 +0530

    Kickme Plugin Created

--- src/main/java/com/anie/Plugins/kickme.java
+++ src/main/java/com/anie/Plugins/kickme.java
@@ -2,15 +2,23 @@ package com.anie.Plugins;
 
 import com.anie.Anie;
 import com.anie.Master;
-
+import org.telegram.telegrambots.meta.api.methods.groupadministration.KickChatMember;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class kickme extends Anie implements Master {
-
     @Override
     public void handleRequest(Update update, String cmd) {
-        
-        
+        if(cmd.equalsIgnoreCase(handler()+"kickme")) {
+            long uid = update.getMessage().getFrom().getId();
+
+            KickChatMember kickChatMember = new KickChatMember(chatId(update), uid);
+            try {
+                execute(kickChatMember);
+                sendMessage(update, "Successfully Kicked " + update.getMessage().getFrom().getUserName());
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
     }
-    
 }
