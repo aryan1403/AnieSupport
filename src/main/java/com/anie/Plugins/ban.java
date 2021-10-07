34785b1dcf3b502b4008eb72d33d8b36a2c65fe5
Aaryan <aaryan14032006@gmail.com>
2021-10-07 13:18:09 +0530

    Created Ban Plugin

--- src/main/java/com/anie/Plugins/ban.java
+++ src/main/java/com/anie/Plugins/ban.java
@@ -1,14 +1,43 @@
 package com.anie.Plugins;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.anie.Anie;
 import com.anie.Master;
+import org.telegram.telegrambots.meta.api.methods.groupadministration.BanChatMember;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class ban extends Anie implements Master{
 
     @Override
     public void handleRequest(Update update, String cmd) {
-        
+        if(cmd.equalsIgnoreCase(handler()+"ban") && update.getMessage().isReply()) {
+            long uid = update.getMessage().getReplyToMessage().getFrom().getId();
+
+            BanChatMember member = new BanChatMember(chatId(update), uid);
+
+            List<List<InlineKeyboardButton>> bList = new ArrayList<List<InlineKeyboardButton>>();
+            InlineKeyboardButton button = new InlineKeyboardButton("🚫 Unban");
+            button.setCallbackData("ban"+uid);
+            List<InlineKeyboardButton> buttons = new ArrayList<>();
+            buttons.add(button);
+            bList.add(buttons);
+            InlineKeyboardMarkup markup = new InlineKeyboardMarkup(bList);
+
+            try {
+                execute(member);
+                SendMessage message = new SendMessage(chatId(update), "Successfully Banned "+ update.getMessage().getReplyToMessage().getFrom().getUserName());
+                message.setReplyMarkup(markup);
+                execute(message);
+            } catch (TelegramApiException e) {
+                sendMessage(update, e.getMessage());
+            }
+        }
     }
     
 }
