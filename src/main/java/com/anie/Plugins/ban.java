7e1841a72e85143a3c6056e1e5149e90773abe20
Aaryan <aaryan14032006@gmail.com>
2021-10-07 12:48:25 +0530

    Created ban Plugin

---
+++ src/main/java/com/anie/Plugins/ban.java
@@ -0,0 +1,14 @@
+package com.anie.Plugins;
+
+import com.anie.Anie;
+import com.anie.Master;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class ban extends Anie implements Master{
+
+    @Override
+    public void handleRequest(Update update, String cmd) {
+        
+    }
+    
+}
