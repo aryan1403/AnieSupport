8bbffbc64f934996f9aed8d33604b043378a279a
Aaryan <aaryan14032006@gmail.com>
2021-10-07 13:18:23 +0530

    Created CallBackHandler

---
+++ src/main/java/com/anie/Plugins/CallBacks/handleCallBack.java
@@ -0,0 +1,29 @@
+package com.anie.Plugins.CallBacks;
+
+import com.anie.Anie;
+import com.anie.Master;
+
+import org.telegram.telegrambots.meta.api.methods.groupadministration.UnbanChatMember;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class handleCallBack extends Anie implements Master {
+
+    @Override
+    public void handleRequest(Update update, String cmd) {
+        if(update.getCallbackQuery().getData().startsWith("ban")) {
+            long uid = Integer.parseInt(update.getCallbackQuery().getData().substring(4));
+
+            UnbanChatMember unbanChatMember = new UnbanChatMember(chatId(update), uid);
+
+            try {
+                execute(unbanChatMember);
+                sendMessage(update, "Successfully Unbanned!\nI wish he would not do the same Again ;)");
+            } catch (TelegramApiException e) {
+                sendMessage(update, e.getMessage());
+            }
+        }
+        
+    }
+    
+}
